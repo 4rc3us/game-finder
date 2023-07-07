e945acdb7d1cd5fe308c15b5c4cbfa75234587c2
Juan Daniel Arce <juandanielarce398@gmail.com>
2023-07-07 00:03:03 -0500

    Order by name pipeline

--- GameFinder/pipelines.py
+++ GameFinder/pipelines.py
@@ -1,13 +1,15 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
-class GamefinderPipeline:
+class OrderPipeline:
+    def __init__(self):
+        self.items = None
+    
+    def open_spider(self, spider):
+        self.items = []
+    
     def process_item(self, item, spider):
+        self.items.append(item)
         return item
+
+    def close_spider(self, spider):
+        self.items.sort(key=lambda x: x['title'])
+        for item in self.items:
+            print(item)
