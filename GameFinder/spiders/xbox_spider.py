146fb72461d3f4bd46d94c8bada82fb1b7025a63
Juan Daniel <juandanielarce398@gmail.com>
2023-08-28 16:10:15 -0500

    New xbox scraper

--- GameFinder/spiders/xbox_spider.py
+++ GameFinder/spiders/xbox_spider.py
@@ -1,61 +1,47 @@
-import json
-import unicodedata
-
 import scrapy
 
 from GameFinder.Builders.XboxUrlBuilder import XboxUrlBuilder
 from GameFinder.items import GameItem
 
 
+def normalize_price(price: str):
+    if price is None:
+        return "not found"
+    
+    return price.replace("$", "").replace(".", "").strip()
+
+
 class XboxSpider(scrapy.Spider):
     name = "xbox"
-    base_address = "https://www.xbox.com/es-CO/search/results/games"
+    base_address = "https://www.xbox.com/es-CO/search"
     game = None
     
     def start_requests(self):
         self.game = getattr(self, "game", None)
-        platforms = getattr(self, "platforms", "xboxseries")
         
         builder = XboxUrlBuilder(self.base_address)
         
-        url = builder.add_platforms(platforms).add_game(self.game).build()
+        url = builder.add_game(self.game).build()
         
         yield scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response, **kwargs):
-        game_data = response.css("head script::text").get().strip()
-        results_list = self.find_json(game_data)["core2"]["products"]["productSummaries"]
+        body = response.css("body")
+        context_search = body.css("#primaryArea div div div")
+        games = context_search.css("div:nth-child(3) section")
         
-        for game in results_list:
-            game_title = game["title"]
-            game_sku = game["specificPrices"]["purchaseable"][0]["skuId"]
-            game_id = game["productId"]
-            game_photos = [{"url": img["url"], "width": img["width"], "height": img["height"]}
-                           for img in game["images"].values()]
-            
+        for game in games:
             game_item = GameItem()
+            game_title = game.css("span.x-heading::text").get()
+            game_photo = 'http:' + game.css("img.c-image::attr(src)").get()
+            game_price = game.css("div.x-price span::text").get()
+            game_url = game.css("a::attr(href)").get()
+            
             game_item["title"] = game_title
-            game_item["price"] = game["specificPrices"]["purchaseable"][0]["listPrice"]
-            game_item["link"] = self.build_game_link(game_title, game_id, game_sku)
+            game_item["price"] = normalize_price(game_price)
+            game_item["link"] = game_url
             game_item["store"] = "xbox"
-            game_item["photos"] = game_photos
+            game_item["photos"] = [game_photo]
             game_item["exchange"] = "COP"
             
             yield game_item
-    
-    @staticmethod
-    def find_json(text: str):
-        start_index = text.find('{')
-        end_index = text.find('};', start_index) + 1
-        json_string = text[start_index:end_index]
-        
-        data = json.loads(json_string)
-        product_summaries = [value for value in data["core2"]["products"]["productSummaries"].values()]
-        data["core2"]["products"]["productSummaries"] = product_summaries
-        
-        return data
-    
-    @staticmethod
-    def build_game_link(game_title: str, game_id: str, sku_id: str):
-        normalized_text = unicodedata.normalize('NFKD', game_title).encode('ascii', 'ignore').decode('utf-8')
-        return f"https://www.xbox.com/es-CO/games/store/{normalized_text.replace(' ', '-')}/{game_id}/{sku_id}"
